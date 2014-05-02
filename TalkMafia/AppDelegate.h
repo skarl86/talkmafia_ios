9bad123b4271d71f2ce47a8cb2261808ce9b0bfd
NK <beohemian@gmail.com>
2014-05-03 03:14:53 +0900

    Initial Commit

---
+++ TalkMafia/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TalkMafia
+//
+//  Created by NCri on 2014. 5. 3..
+//  Copyright (c) 2014년 NCri Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TalkMafia/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TalkMafia
+//
+//  Created by NCri on 2014. 5. 3..
+//  Copyright (c) 2014년 NCri Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
